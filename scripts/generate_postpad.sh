ddc0a4130d3baedb6ca60234aa6fcbe9f94134e5
Brad Heller <brad@aboutus.org>
2011-05-07 22:28:30 -0700

    Small comment change

--- scripts/generate_postpad.sh
+++ scripts/generate_postpad.sh
@@ -5,6 +5,7 @@ stage_one_sec=`stat -c%s "lib/grub-0.97-i386-pc/boot/grub/stage1"`
 stage_two_size=`stat -c%s "lib/grub-0.97-i386-pc/boot/grub/stage2"`
 export total=`expr 1474560 - $kernel_size - $stage_one_sec - $stage_two_size`
 
+# Make sure we aren't doing something crazy...
 if [[ -f "lib/postpad"]] && [[$total -ne `stat -c%s "lib/postpad"` ]]; then
 	echo "Post padding size: $total"
 	perl -e 'print "\xFF"x$ENV{"total"}' > lib/postpad

1e365b73ba2e047ce04091144378889447f91bf3
Brad Heller <brad@cloudability.com>
2013-03-31 12:29:10 -0700

    helpers + cleanup

--- src/types.h
+++ src/types.h
@@ -8,6 +8,9 @@ typedef int BOOL;
 
 #define UNUSED(expr) do { (void)(expr); } while (0)
 
-#define NULL    0
-#define TRUE    1
-#define FALSE   0
+#define TRUE            (BOOL)1
+#define FALSE           (BOOL)0
+#define NULL            0
+
+#define AS_BOOL(x)      x ? TRUE : FALSE
+#define NOT_NULL(x)     x != NULL
